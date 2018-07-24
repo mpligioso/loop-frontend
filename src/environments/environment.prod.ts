f1e149fdfe17dd03b352abf65286c6e46ff13783
Marie Gillier <magillier@hotmail.fr>
2018-07-24 15:55:26 +0200

    autocomplete prod mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // PRODUCTION backend URL is just the empty string (same domain)
   backendUrl: "",
-  GOOGLE_MAP_API_KEY: "AIzaSyAw9MA2YwQlhDywGiTNQqEJrAJVRS1ZCZg"
+  GOOGLE_MAP_API_KEY: "AIzaSyBxp25VtQir86dy8rbjpQCAo4XtTMw8TzM"
 };
