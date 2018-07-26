e11ff958f879d51c4f6bc128af32c5450133e22b
Marie Gillier <magillier@hotmail.fr>
2018-07-26 17:19:26 +0200

    setting up gm direction in create trip component but really not working yet

--- src/app/trip-details-page/trip-details-page.component.ts
+++ src/app/trip-details-page/trip-details-page.component.ts
@@ -1,4 +1,7 @@
+/// <reference types="googlemaps" />
 import { Component, OnInit } from '@angular/core';
+import { MapsAPILoader } from "@agm/core";
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-trip-details-page',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trip-details-page.component.css']
 })
 export class TripDetailsPageComponent implements OnInit {
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
+  directionsService = new google.maps.DirectionsService;
+  directionsDisplay = new google.maps.DirectionsRenderer;
+  directionsDisplay2 = new google.maps.DirectionsRenderer;
 
   constructor() { }
 
   ngOnInit() {
-  }
+    var mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  };
+    // directionsDisplay.setMap(map);
+    // directionsDisplay2.setMap(map);
+  
 
 }
