c483aaffe69c327a8c46c446b2963c86fabb6b97
Meri-Pier Ligioso <mpligioso@gmail.com>
2018-07-26 03:44:16 +0200

    link between requesting a match done. new bugs relating to displaying dynamic variables

--- src/app/requests-page/requests-page.component.ts
+++ src/app/requests-page/requests-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Match, MatchService } from '../api/match.service';
+import { User, AuthService } from '../api/auth.service';
 
 @Component({
   selector: 'app-requests-page',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./requests-page.component.css']
 })
 export class RequestsPageComponent implements OnInit {
+  matchData: Array<Match> = [];
+  userData: User;
 
-  constructor() { }
+  constructor(
+    public myAuthServ: AuthService,
+    private myMatchServ: MatchService
+  ) { }
 
   ngOnInit() {
+    this.getUserMatchRequests();
+    this.getUserInfo();
+  }
+
+  getUserMatchRequests(){
+    this.myMatchServ.getMatchRequests()
+      .then((results: Array<Match>) => {
+        console.log(results)
+        this.matchData = results;
+        console.log(results[0].acceptedByDriver)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+  }
+
+  getUserInfo() {
+    this.myAuthServ
+      .check()
+      .then((response: any) => {
+        console.log(response)
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
 }
