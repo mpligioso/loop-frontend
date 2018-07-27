633d514174e1d213d48797afafb3425592d66a73
Manelle_G;` <manelle.gueriouz@gmail.com>
2018-07-27 14:40:20 +0200

    css dashboard

--- src/app/requests-page/requests-page.component.ts
+++ src/app/requests-page/requests-page.component.ts
@@ -1,37 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { Match, MatchService } from '../api/match.service';
-import { User, AuthService } from '../api/auth.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { Match, MatchService } from "../api/match.service";
+import { User, AuthService } from "../api/auth.service";
+import { Router } from "@angular/router";
+import { faHourglassHalf } from "../../../node_modules/@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-requests-page',
-  templateUrl: './requests-page.component.html',
-  styleUrls: ['./requests-page.component.css']
+  selector: "app-requests-page",
+  templateUrl: "./requests-page.component.html",
+  styleUrls: ["./requests-page.component.css"]
 })
 export class RequestsPageComponent implements OnInit {
   matchData: Array<Match> = [];
   userData: User;
   match: Match;
   matchId: string;
+  faHourglassHalf = faHourglassHalf;
 
   constructor(
     public myAuthServ: AuthService,
     private myMatchServ: MatchService,
     private myRouterServ: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.getUserMatchRequests();
     this.getUserInfo();
   }
 
-  getUserMatchRequests(){
-    this.myMatchServ.getMatchRequests()
+  getUserMatchRequests() {
+    this.myMatchServ
+      .getMatchRequests()
       .then((results: Array<Match>) => {
         this.matchData = results;
       })
-      .catch((err) => {
-        console.log(err)
+      .catch(err => {
+        console.log(err);
       });
   }
 
@@ -40,44 +43,45 @@ export class RequestsPageComponent implements OnInit {
       .check()
       .then((response: any) => {
         this.userData = response;
-        console.log(response.isDriver)
+        console.log(response.isDriver);
       })
       .catch(err => {
         console.log(err);
       });
   }
 
-  removeMatch(matchId){
+  removeMatch(matchId) {
     const isOkay = confirm("Voulez-vous enlever la demande?");
 
-    if(isOkay){
-      this.myMatchServ.deleteMatch(matchId)
-        .then((response) => {
+    if (isOkay) {
+      this.myMatchServ
+        .deleteMatch(matchId)
+        .then(response => {
           this.myRouterServ.navigateByUrl("/dashboard");
-          alert("Le match a été supprimé")
+          alert("Le match a été supprimé");
         })
-        .catch((err) => {
+        .catch(err => {
           console.log(err);
-        })
-      }
-  }
-
-  acceptMatch(matchId){
-    const isOkay = confirm("Voulez-vous accepter la demande?");
-
-    const { isDriver } = this.userData;
-
-    if(isOkay){
-      this.myMatchServ.changeMatchStatus(matchId, isDriver)
-        .then((response) => {
-          this.myRouterServ.navigateByUrl("/dashboard");
-          alert("Le match a été confirmé")
-        })
-        .catch((err) => {
-          alert("Oups! Votre demande ne peut pas être confirmé.")
-          console.log(err)
         });
     }
   }
 
+  acceptMatch(matchId) {
+    const isOkay = confirm("Voulez-vous accepter la demande?");
+
+    const { isDriver } = this.userData;
+
+    if (isOkay) {
+      this.myMatchServ
+        .changeMatchStatus(matchId, isDriver)
+        .then(response => {
+          this.myRouterServ.navigateByUrl("/dashboard");
+          alert("Le match a été confirmé");
+        })
+        .catch(err => {
+          alert("Oups! Votre demande ne peut pas être confirmé.");
+          console.log(err);
+        });
+    }
+  }
 }
